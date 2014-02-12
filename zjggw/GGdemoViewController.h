9e1810870a9b1a9aac4bc7cb31cc9e1e241b562a
yzpniceboy <yzpniceboy@gmail.com>
2014-02-13 07:54:39 +0800

    完成首页图片幻灯片展示，布局设置，集成ASIHTTP框架

---
+++ zjggw/GGdemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGdemoViewController.h
+//  zjggw
+//
+//  Created by yzp on 14-2-12.
+//  Copyright (c) 2014年 yzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGdemoViewController : UIViewController
+
+@end
---
+++ zjggw/GGdemoViewController.m
@@ -0,0 +1,38 @@
+//
+//  GGdemoViewController.m
+//  zjggw
+//
+//  Created by yzp on 14-2-12.
+//  Copyright (c) 2014年 yzp. All rights reserved.
+//
+
+#import "GGdemoViewController.h"
+
+@interface GGdemoViewController ()
+
+@end
+
+@implementation GGdemoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
